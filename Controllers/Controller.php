782a3f91e9c6ee3d76a8dd10a055b78cb18434ca
SergProgMan <semibratyev.com>
2018-11-28 11:00:55 +0200

    repare menu

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -12,8 +12,18 @@ class Controller extends Database {
         echo "not auth";
     }
 
+    public static function createMenu(){
+        if(self::$userLogin){
+            require_once("./Views/MenuFull.php");
+        }else {
+            require_once("./Views/Menu.php");
+        }
+}
+    
+
     public static function CreateView($viewName){
         self::checkAuth();
+        self::createMenu();
         require_once("./Views/$viewName.php");
     }
 }
---
+++ Views/Menu.php
@@ -0,0 +1,6 @@
+
+<ul>
+    <li><a class="active" href="main.php">Home</a></li>
+    <li><a href="login.php">Login</a></li>
+    <li><a href="registration.php">Registration</a></li>
+</ul>
